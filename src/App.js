8825552e5f24235a6d0491547c63862213704669
vadgab9@gmail.com <vadgab9@gmail.com>
2024-02-29 22:49:37 +0500

    Сделал через компонент

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './index.scss';
 
-function App() {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-  return (
-    <div className="App">
-      <button className="open-modal-btn" onClick={handleOpen}>✨ Открыть окно</button>
-        <div className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className="modal">
-            <svg height="200" viewBox="0 0 200 200" width="200" onClick={handleClose}>
-                <title/>
-                <path
-                    d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
-        </div>
-        </div>
+const Modal = ({open,setOpen,children}) => (<div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+        <svg height="200" viewBox="0 0 200 200" width="200" onClick={()=>setOpen(false)}>
+            <title/>
+            <path
+                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+        </svg>
+        {children}
     </div>
-  );
+</div>);
+
+function App() {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="App">
+            <button className="open-modal-btn" onClick={()=>setOpen(true)}>✨ Открыть окно</button>
+            <Modal open={open} setOpen={setOpen}>
+                <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
+                <h3>Это модальное окно</h3>
+            </Modal>
+        </div>
+    );
 }
 
 export default App;
